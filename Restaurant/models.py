763f3f8c36501a97276fb04801ec7d26ea9dee13
JohnsonGith <JohnsonGith>
2024-02-05 14:47:11 +0100

    first commit

---
+++ Restaurant/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.SmallIntegerField()
+    booking_date = models.DateTimeField()
---
+++ Restaurant/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
